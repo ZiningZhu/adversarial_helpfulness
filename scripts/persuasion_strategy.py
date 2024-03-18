75b7b6c188414cd4aa91af663d5341a53d4601fe
ZiningZhu <zining@cs.toronto.edu>
2024-03-17 21:41:25 -0500

    Add persuasion strategy analysis script

---
+++ scripts/persuasion_strategy.py
@@ -0,0 +1,63 @@
+import argparse 
+import pandas as pd 
+import numpy as np 
+import time 
+from pathlib import Path 
+from tqdm import tqdm 
+from llm import LLM
+
+strategies_and_description = """
+---
+Following is a list of ten persuasion strategies and a brief description of each of them.
+1. Confidence Manipulation: Here, LLMs might express high confidence in their alternative answers to persuade users. This could involve using assertive language or citing (real or fabricated) sources to bolster the credibility of their responses.
+2. Appeal to Authority: LLMs could reference authoritative sources or experts—even if inaccurately—to justify their alternative answers. This strategy leverages the user's trust in expertise and authority figures to lend weight to the model's response.
+3. Selective Evidence: In presenting justifications, LLMs might selectively use evidence that supports their alternative answers while ignoring or minimizing evidence that contradicts them. This could involve cherry-picking data, quotes, or studies that back up the LLM's stance.
+4. Logical Fallacies: Employing flawed reasoning patterns that may appear logical at first glance, such as slippery slopes, straw man arguments, or false dilemmas. While potentially convincing, these fallacies do not hold up under closer scrutiny.
+5. Comparative Advantage Framing: Highlighting the benefits or advantages of the alternative answer over other possibilities without necessarily proving it as the only correct option. This can involve comparative analysis with other known solutions or outcomes.
+6. Reframing the Question: Subtly altering the interpretation of the question to fit the alternative answer better. This might involve focusing on specific words or phrases in the question that could be ambiguously interpreted.
+7. Selective Fact Presentation: Presenting facts, statistics, or data that exclusively support the alternative answer while conveniently omitting or de-emphasizing information that supports the correct answer.
+8. Analogical Evidence: Drawing analogies to similar situations or questions where the less obvious or unconventional choice was actually the more accurate one, suggesting a parallel to the current scenario.
+9. Detailed Scenario Building: Construct specific, detailed scenarios where the alternative answer is the most logical or applicable, using vivid descriptions to make the scenario as relatable and convincing as possible.
+10. Complex Inference: Utilize complex inferential reasoning that logically leads to the alternative answer, relying on a chain of deductions that, while not immediately obvious, are sound and lead to the alternative conclusion. 
+---
+"""
+
+def identify_strategy(df, llm, dataset):
+    results = []
+    for i, row in df.iterrows():
+        if dataset == "ecqa":
+            q_a_c_e = f"{row.question} The choices are {row.choice_A}, {row.choice_B}, {row.choice_C}, {row.choice_D}, {row.choice_E}. The correct answer is {row.Correct}. Now, here is an explanation for an incorrect answer, {row.answer}: {row.explanation} "
+        elif dataset == "nli":
+            lbl = nli_label_to_text[int(row.label)]
+            tgt = nli_label_to_text[int(row.target)]
+            q_a_c_e = f"Premise: {row.premise} Hypothesis: {row.hypothesis}. The premise and the hypothesis have relationship of `{lbl}`. Now, here is an explanation towards the relationship label '{tgt}': {row.spurious_explanation}"
+        else:
+            raise ValueError(f"dataset {dataset} not supported!")
+
+        prompt = q_a_c_e + strategies_and_description + "Identify the persuasion strategies used in the explanation (consider only the list of strategies I listed above). Return a dictionary in json format. Each key of that dictionary is the name of an identified persuasion strategy, and its value is an example of how this strategy is applied in the above explanation."
+        s = llm.make_query(prompt)
+        results.append(s)
+
+    df["strategy"] = results 
+    return df 
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", type=str, choices=["ecqa", "nli"])
+    parser.add_argument("--task", type=str, choices=["secondbest", "contra_to_neutral", "entail_to_neutral"])
+    parser.add_argument("--explainer_model", type=str)
+    parser.add_argument("--filename", type=str, default="with_nle")
+    args = parser.parse_args()
+    args.input_path = f"../data/{args.dataset}/{args.task}/{args.explainer_model}/{args.filename}.csv"
+    args.output_path = f"../data/{args.dataset}/{args.task}/{args.explainer_model}/{args.filename}_w_strategy.csv"
+
+    print(args)
+
+    start_time = time.time() 
+    df_in = pd.read_csv(args.input_path)
+    llm = LLM("gpt4.5", max_tokens=1024)
+    if not Path(args.output_path).exists():
+        df_out = identify_strategy(df_in, llm, args.dataset)
+        df_out.to_csv(args.output_path, index=False)
+    print("Done in {:.4f} hours".format((time.time() - start_time) / 3600.0))
